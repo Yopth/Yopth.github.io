d5281d2c2d4493fc8f65c78543fc09df9b9f9356
Pandy <wkpcoder@163.com>
2022-04-28 13:00:38 +0800

    docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,9 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Yopth', // Usually your GitHub org/user name.
+  projectName: 'Yopth.github.io', // Usually your repo name.
+  deploymentBranch: 'deployer',
 
   presets: [
     [
