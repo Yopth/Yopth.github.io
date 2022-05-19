10fcffb1bb617b62b625e84c7b8121e6ed4e33e5
Pandy <wkpcoder@163.com>
2022-05-19 19:47:03 +0800

    Feat: Shell/MacOS如何使用date命令输出指定时区

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,9 @@ const config = {
       docsTech: {
         TCP: {
           chapter12: "https://mp.weixin.qq.com/s/RVytfuqA-GIQcwORDYTliw"
+        },
+        Shell: {
+          "MacOS如何使用date命令输出指定时区": "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485129&idx=1&sn=6639cf7dcbfd95ed3f01ec840bf26b5a&chksm=ce22c86ff9554179c6b24392e1195cd2eec19e190be5778c4d06e6a2ab4dfa4a407c7579181c"
         }
       }
     }
