e1bd6828e3dfea59edcdee3cf55fbc138f93403f
LAPTOP-ESTD7GL5\Pandy <wkpcoder@163.com>
2022-05-21 00:42:11 +0800

    docs: 必知必会margin-inline, margin-block, padding-inline, padding-block

--- src/links/docs-tech.ts
+++ src/links/docs-tech.ts
@@ -1,7 +1,9 @@
 const CSS = {
   ["CSS的Cascade"]:
     "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485132&idx=1&sn=f19036b01c636bcafac9404e6703f1c3&chksm=ce22c86af955417c820bb1bb474e01156d47d8aeb60f08e25cef02706493a3775779ecc2ca4a",
-};
+  ["必知必会margin-inline, margin-block, padding-inline, padding-block"]:
+    "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&tempkey=MTE2Nl93S0t0UTVqM1ExeFQzZUZLTGg5UWtYUjFEWjhOSUZVV19fR3A5UW4zUUF5QktLekpyLU01MFE0U0g1YUw3TTlpcXBCbXh0TDFsbEVxMmpzVzdZdzhuTUs0cDBTTzZwaUktdTFEVEd1c1AxRUtDZXpPMG8xaFBBUkF6R05Cd1VpQUNyOEdtM1hNOS02NEgtczJ1dXNYdWJRSGlnQWlpSU1wWEZwS3dnfn4%3D&chksm=ce22c87cf955416adf73fb62779a66bea36a1eb4a657a363846676d5fd8329d53ef60e34ca19"
+  };
 const TCP = {
   chapter12: "https://mp.weixin.qq.com/s/RVytfuqA-GIQcwORDYTliw",
 };
