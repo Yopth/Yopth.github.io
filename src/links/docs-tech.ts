a3b08a56d08787d28d5fd341cc7ffa1496a8edae
Pandy <wkpcoder@163.com>
2022-06-15 16:54:13 +0800

    Feat: migrate css in depth

--- src/links/docs-tech.ts
+++ src/links/docs-tech.ts
@@ -1,17 +1,34 @@
+import Platform from './platform';
+
 const CSS = {
-  ["CSS的Cascade"]:
-    "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485132&idx=1&sn=f19036b01c636bcafac9404e6703f1c3&chksm=ce22c86af955417c820bb1bb474e01156d47d8aeb60f08e25cef02706493a3775779ecc2ca4a",
-  ["必知必会margin-inline, margin-block, padding-inline, padding-block"]:
-    "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&tempkey=MTE2Nl93S0t0UTVqM1ExeFQzZUZLTGg5UWtYUjFEWjhOSUZVV19fR3A5UW4zUUF5QktLekpyLU01MFE0U0g1YUw3TTlpcXBCbXh0TDFsbEVxMmpzVzdZdzhuTUs0cDBTTzZwaUktdTFEVEd1c1AxRUtDZXpPMG8xaFBBUkF6R05Cd1VpQUNyOEdtM1hNOS02NEgtczJ1dXNYdWJRSGlnQWlpSU1wWEZwS3dnfn4%3D&chksm=ce22c87cf955416adf73fb62779a66bea36a1eb4a657a363846676d5fd8329d53ef60e34ca19"
-  };
+  ['CSS的Cascade']:
+    'https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485132&idx=1&sn=f19036b01c636bcafac9404e6703f1c3&chksm=ce22c86af955417c820bb1bb474e01156d47d8aeb60f08e25cef02706493a3775779ecc2ca4a',
+  ['必知必会margin-inline, margin-block, padding-inline, padding-block']:
+    'https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&tempkey=MTE2Nl93S0t0UTVqM1ExeFQzZUZLTGg5UWtYUjFEWjhOSUZVV19fR3A5UW4zUUF5QktLekpyLU01MFE0U0g1YUw3TTlpcXBCbXh0TDFsbEVxMmpzVzdZdzhuTUs0cDBTTzZwaUktdTFEVEd1c1AxRUtDZXpPMG8xaFBBUkF6R05Cd1VpQUNyOEdtM1hNOS02NEgtczJ1dXNYdWJRSGlnQWlpSU1wWEZwS3dnfn4%3D&chksm=ce22c87cf955416adf73fb62779a66bea36a1eb4a657a363846676d5fd8329d53ef60e34ca19',
+};
+
+const CSSInDepth = {
+  ['CSS In Depth: PART1 REVIEWING THE FUNDAMENTALS']:
+    'https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&tempkey=MTE3MF9aaG5pVGpEYUIyWHhMSHJqdEljSkExd1BFYm5Dd3JWa19Yd2VaY0k2SDAtV25IejlOd2NYXzlrUmU0Z25VN04yS0N6WXhvVG1RSHpwS3BuNDhxSDFKZUVCOEVCYi1jRXkzMGtkVHZKOFlobHBaQWlMbFA2ZzhCZ2RBeVBSdjJOOFhfd01vVEJhMmdOQUpja2hXQmNRYlNRZ3BVWERuOFpIdUdIX293fn4%3D&chksm=ce22c986f9554090ad659f97300bc3cd82b4b337a054e7b7c4a04364e8101a3799283b0c8f09&token=747278028&lang=en_US#rd',
+  [Platform.CSDN]: {
+    ['CSS In Depth: PART1 REVIEWING THE FUNDAMENTALS']:
+      'https://blog.csdn.net/YopenLang/article/details/125299678?csdn_share_tail=%7B%22type%22%3A%22blog%22%2C%22rType%22%3A%22article%22%2C%22rId%22%3A%22125299678%22%2C%22source%22%3A%22YopenLang%22%7D&ctrtid=CQcQU',
+  },
+  [Platform.JueJin]: {
+    ['CSS In Depth: PART1 REVIEWING THE FUNDAMENTALS']:
+      'https://juejin.cn/post/7109386323173048327/',
+  },
+};
+
 const TCP = {
-  chapter12: "https://mp.weixin.qq.com/s/RVytfuqA-GIQcwORDYTliw",
+  chapter12: 'https://mp.weixin.qq.com/s/RVytfuqA-GIQcwORDYTliw',
 };
 const Shell = {
-  ["MacOS如何使用date命令输出指定时区"]:
-    "https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485129&idx=1&sn=6639cf7dcbfd95ed3f01ec840bf26b5a&chksm=ce22c86ff9554179c6b24392e1195cd2eec19e190be5778c4d06e6a2ab4dfa4a407c7579181c",
+  ['MacOS如何使用date命令输出指定时区']:
+    'https://mp.weixin.qq.com/s?__biz=Mzg2MDYyNTg2NQ==&mid=2247485129&idx=1&sn=6639cf7dcbfd95ed3f01ec840bf26b5a&chksm=ce22c86ff9554179c6b24392e1195cd2eec19e190be5778c4d06e6a2ab4dfa4a407c7579181c',
 };
 
 export { CSS as CSSLink };
+export { CSSInDepth as CSSInDepthLink };
 export { TCP as TCPLink };
 export { Shell as ShellLink };
--- src/links/platform.ts
+++ src/links/platform.ts
@@ -1,6 +1,7 @@
 enum Platform {
   WeChat = 'WeChat Official Account',
   JueJin = 'Jue Jin',
+  CSDN = 'CSDN',
 }
 
 export default Platform;
